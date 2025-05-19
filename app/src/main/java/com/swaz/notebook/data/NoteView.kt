91b0d2ae67be2afc077e7b0c7fe4f2a70014d9dc
Swaz.10 <arnoldswaz.10@proton.me>
2025-05-19 16:32:20 +0300

    p

--- app/src/main/java/com/swaz/notebook/data/NoteView.kt
+++ app/src/main/java/com/swaz/notebook/data/NoteView.kt
@@ -1,5 +1,63 @@
 package com.swaz.notebook.data
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.swaz.notebook.model.Note
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class NoteView : ViewModel
+class NoteView : ViewModel(){
+    private val db = FirebaseFirestore.getInstance()
+    private val ref = db.collection("notes")
+    private val _note = MutableStateFlow<List<Note>>(
+        emptyList()
+    )
+    val notes : StateFlow<List<Note>> = _note
+
+    init {
+        fetchNotes()
+    }
+    private fun fetchNotes()
+    {
+        ref.orderBy("timestamp",
+            Query.Direction.DESCENDING
+
+        )
+            .addSnapshotListener { snapshot, _ ->
+                if (
+                    snapshot != null &&
+                    ! snapshot.isEmpty
+                )
+                {
+                    val list = snapshot.documents.map {
+                        it.toObject( Note::class.java)!!.copy( id = it.id)
+                    }
+                    _note.value = list
+                }
+            }
+    }
+    fun addNote( note : Note)
+    {
+        ref.add(note)
+    }
+    fun updateNote( note : Note)
+    {
+        ref.document(note.id).set(note)
+    }
+    fun deleteNote( id : String )
+    {
+       ref.document(id).delete()
+    }
+    fun getNoteById(
+        id: String,
+        onResult : (Note?) -> Unit
+    )
+    {
+        ref.document(id).get()
+            .addOnSuccessListener {
+                val note = it.toObject(Note::class.java)
+                    ?.copy( id = it.id)
+            }
+    }
+}
--- app/src/main/java/com/swaz/notebook/ui/theme/screens/List.kt
+++ app/src/main/java/com/swaz/notebook/ui/theme/screens/List.kt
@@ -1,2 +1,99 @@
 package com.swaz.notebook.ui.theme.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.swaz.notebook.data.NoteView
+import com.swaz.notebook.model.Note
+
+@Composable
+fun Lst(
+    viewModel : NoteView,
+    onAddNote : () -> Unit,
+    onEditNote : (String) -> Unit,
+
+    )
+{
+    val notes by viewModel.notes.collectAsState()
+    Scaffold(
+        floatingActionButton =
+            {
+                FloatingActionButton(onClick = onAddNote)
+                {
+                    Icon(
+                        Icons.Default.Add,
+                        contentDescription = "Add"
+                    )
+                }
+            }
+    )
+    { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(padding)
+        )
+        {
+            items(notes){ note ->
+                Itm(
+                    note = note,
+                    onClick = { onEditNote(note.id)}
+                )
+
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun Itm(
+    note: Note,
+    onClick : () -> Unit
+)
+{
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { onClick() }
+    )
+    {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        {
+            Text(
+                note.title,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                note.content.take(100),
+                style = MaterialTheme.typography.bodyMedium
+
+            )
+        }
+    }
+
+}
+
